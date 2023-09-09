21043468debf5d76ffbda0b6159fae373177fb13
DoganSenol <senoldogan2018@gmail.com>
2023-09-09 16:15:39 +0200

    TS03 Murat elementler Elements adli Class eklendi

--- src/Main.java
+++ src/Main.java
@@ -66,38 +66,25 @@ public class Main extends BaseDriver {
     @Test
     public void TS03() {
         driver.get("https://techno.study/tr");
+
+        Elements elements=new Elements();
         MyFunc.Bekle(3);
 
-        WebElement basvur = driver.findElement(By.xpath("(//*[text()='BAŞVUR'])[1]"));
-        basvur.click();
-        WebElement adSoyad = driver.findElement(By.name("name"));
-        adSoyad.sendKeys(adSoy);
-        WebElement email = driver.findElement(By.name("email"));
-        email.sendKeys(mail);
-        WebElement ulkekod = driver.findElement(By.cssSelector("[class='t-input-phonemask__select-flag']"));
-        ulkekod.click();
-        WebElement ulkeKodu = driver.findElement(By.id("t-phonemask_ad"));
-        ulkeKodu.click();
-        WebElement tlf = driver.findElement(By.name("tildaspec-phone-part[]"));
-        tlf.sendKeys(tl);
-        WebElement ul = driver.findElement(By.id("sb-1667664755026"));
-        ul.click();
-        WebElement ulke = driver.findElement(By.xpath("//option[text()='Andorra']"));
-        ulke.click();
-        WebElement kursSec = driver.findElement(By.id("sb-1663337581601"));
-        kursSec.click();
-        WebElement kurs = driver.findElement(By.xpath("//option[text()='Job Center & Arbeitsamt']"));
-        kurs.click();
-        WebElement nereden = driver.findElement(By.id("sb-1670423010572"));
-        nereden.click();
-        WebElement nerdenBul = driver.findElement(By.xpath("//option[text()='Arkadaş vasıtası ile']"));
-        nerdenBul.click();
-        WebElement promoKod = driver.findElement(By.name("promo code"));
-        promoKod.sendKeys("xyz");
-        WebElement sartname = driver.findElement(By.cssSelector("[class='t-checkbox__indicator']"));
-        sartname.click();
-        WebElement gonder = driver.findElement(By.cssSelector("[class='t-submit']"));
-        gonder.click();
+        elements.basvur.click();
+        elements.adSoyad.sendKeys(adSoy);
+        elements.email.sendKeys(mail);
+        elements.ulkekod.click();
+        elements.ulkeKodu.click();
+        elements.tlf.sendKeys(tl);
+        elements.ul.click();
+        elements.ulke.click();
+        elements.kursSec.click();
+        elements.kurs.click();
+        elements.nereden.click();
+        elements.nerdenBul.click();
+        elements.promoKod.sendKeys("xyz");
+        elements.sartname.click();
+        elements.gonder.click();
         WebElement dogrulama = driver.findElement(By.xpath("(//span[text()='Başvurunuz alınmıştır. Bilgilendirme emailinize gönderilmiştir.'])[2]"));
 
         Assert.assertTrue(dogrulama.getText().contains("Başvurunuz alınmıştır."), "Dogrulanamadi");
